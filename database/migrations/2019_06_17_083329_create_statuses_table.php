4b365c8b78a6d1bb22f061de431b46b4654b8fa1
Brian <303243144@qq.com>
2019-06-17 08:39:34 +0000

    微博数据模型

---
+++ database/migrations/2019_06_17_083329_create_statuses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statuses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('content');
+            $table->integer('user_id')->index();
+            $table->index(['created_at']);
+            // timestamps方法会为微博数据表生成一个微博创建时间字段 created_at和一个微博更新时间字段updated_at
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statuses');
+    }
+}
